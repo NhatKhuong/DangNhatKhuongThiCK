f687cedc542bfd09cf19ace4ce8165186eca1d4f
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-05-27 08:16:30 +0700

    'finshlogin_and_uiBussiness'

---
+++ app/src/main/java/com/example/a19438561_dangnhatkhuong_androiexam/KhoaHoc.java
@@ -0,0 +1,9 @@
+package com.example.a19438561_dangnhatkhuong_androiexam;
+
+import androidx.room.Entity;
+
+import java.io.Serializable;
+
+@Entity(tableName = "khoahoc")
+public class KhoaHoc implements Serializable {
+}
